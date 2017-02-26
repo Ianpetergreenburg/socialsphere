25ab18f2d3b5e3dc4bc8ae1c783229f21f22d547
Ian Peter Greenburg <iangreenburg@yahoo.com>
2017-02-26 01:03:52 -0800

    ajaxed

--- hackathon-starter/public/js/lib/insert.js
+++ hackathon-starter/public/js/lib/insert.js
@@ -11,9 +11,11 @@ $(document).ready(function(){
 		var counter = -1
 		data.posts.forEach(function(post){
 			console.log(counter)
+			var arr = [0,-1.4,-2,-1.4,0]
+			var arr2 = [-2,-1.4,0,1.4,2]
 			counter += 1
 			$('a-scene').append('<a-image class="img-panel" src="' + post.photos[0].alt_sizes[1].url + '"' +
-	 			'width="10" height="10" position="' + (Math.floor(counter/4) - 2) + ' ' + (counter%4 - 2) + ' ' + '-2' + '" rotation="0 ' + '0' + ' 0" scale=".1 .1 .1"></a-image>')
+	 			'width="10" height="10" position="' + arr2[counter%5] + ' ' + (Math.floor(counter/5) - 2) + ' ' + arr[counter%5] + '" rotation="0 ' +  ((counter%5 - 2)*-45) + ' 0" scale=".1 .1 .1"></a-image>')
 		})
 	})
 })
